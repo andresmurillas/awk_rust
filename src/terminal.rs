e136290af4a35a9798b08f14f8137d3f6cb59699
andresmurillas <andresmurillas@protonmail.com>
2022-11-06 17:22:46 +0100

    file exists fallback

--- src/terminal.rs
+++ src/terminal.rs
@@ -4,16 +4,23 @@ use std::fs;
 pub fn get_command() -> Vec<String> {
     let args: Vec<String> = env::args().collect();
 
-    if &args.len() < &2 {
+    if &args.len() < &3 {
         eprintln!("awk [prog] [file]");
         return vec!("end".to_string(), "".to_string());
     }
 
     let prog = &args[1];
 
-    let file = fs::read_to_string(&args[2]).expect("Should have read from file.");
+    let file: String;
 
-    let command: Vec<String> = vec![prog.to_string(), file];
+    if fs::metadata(&args[2]).is_ok() {
+        file = fs::read_to_string(&args[2]).expect("Should have read from file.");
+        let command: Vec<String> = vec![prog.to_string(), file];
 
-    return command;
+        return command;
+    } else {
+        eprintln!("File {} does not exist in current directory.", &args[2]);
+        return vec!("end".to_string(), "".to_string());
+    }
+
 }
